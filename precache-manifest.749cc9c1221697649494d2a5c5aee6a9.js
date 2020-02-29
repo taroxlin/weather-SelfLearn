77bcb3865df5483327821266148c28404e6a020d
Taroxlin <bul41991@gmail.com>
2020-02-29 12:25:07 +0100

    Updates

---
+++ precache-manifest.749cc9c1221697649494d2a5c5aee6a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3efb397f03360d3433c5c601c20830",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "45c5d38f52f1f462bccb",
+    "url": "/weather/static/css/main.3e00f294.chunk.css"
+  },
+  {
+    "revision": "702e7d950fd367dfb625",
+    "url": "/weather/static/js/2.92f38acf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather/static/js/2.92f38acf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45c5d38f52f1f462bccb",
+    "url": "/weather/static/js/main.e652b18b.chunk.js"
+  },
+  {
+    "revision": "273d879ab56f3f03eb32",
+    "url": "/weather/static/js/runtime-main.95c1b2ec.js"
+  },
+  {
+    "revision": "37d378af7a00094c47c1d05f81f1c826",
+    "url": "/weather/static/media/bg.37d378af.jpg"
+  },
+  {
+    "revision": "4be31476ffbfef8c5a3e52529bd64353",
+    "url": "/weather/static/media/selfie.4be31476.png"
+  }
+]);
